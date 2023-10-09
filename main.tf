3b5e85255a0739f9d9bd01ba2e631c5d047aeac4
mehras900 <65251963+mehras900@users.noreply.github.com>
2023-10-09 14:38:03 -0400

    40 terratowns provider (#44)
    * #40 wip- setting up the skeleton for creating a custom terraform provider
    
    * #40 implement terraform provider block
    
    * #40 setting up skeleton for resource
    
    * #40 finished implementation of terraform provider

--- main.tf
+++ main.tf
@@ -13,15 +13,15 @@
 #   # }
 # }
 
-# module "terrahous_aws" {
-#   source = "./modules/terrahous_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_object_path
-#   error_html_filepath = var.error_object_path
-#   content_version = var.content_version
-#   assets_path = var.assets_path
-# }
+  # module "terrahous_aws" {
+  #   source = "./modules/terrahous_aws"
+  #   user_uuid = var.user_uuid
+  #   bucket_name = var.bucket_name
+  #   index_html_filepath = var.index_object_path
+  #   error_html_filepath = var.error_object_path
+  #   content_version = var.content_version
+  #   assets_path = var.assets_path
+  # }
 
 terraform {
     required_providers {
@@ -33,7 +33,20 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+resource "terratowns_home" "gta_home" {
+  name = "This is the GTA 6 game announcement!!!"
+  description = <<DESCRIPTION
+    Grand Theft Auto 6 saw what was easily one of the biggest leaks in gaming history.
+    Roughly 90 videos were posted onto the official GTA Forums by a poster named teapotuberhacker.
+    DESCRIPTION
+  # domain_name   = module.terrahous_aws.cdn_distribution_domain_name
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+
 }
